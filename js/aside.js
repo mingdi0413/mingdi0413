f1d7b4cee437f8cf1b451d16c84708c2a2f920f7
mingdi0413 <117414860+mingdi0413@users.noreply.github.com>
2022-11-16 12:43:03 +0900

    Add files via upload

---
+++ js/aside.js
@@ -0,0 +1,9 @@
+// $(document).ready(function (){
+//     $('.aside >li').click(function (e){
+//         e.preventDefault();
+//         var i = $(this).index();
+
+//         $('.aside > li::before').removeClass('active');
+//         $('.aside > li::before').addClass('active');
+//     })
+// })
